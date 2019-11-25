15f7151df2d6317ab9aab5679ba1d5ef4737d6f0
Ola Vea <ola@olavea.com>
2019-11-25 10:33:20 +0100

    two pages with link back and forth #3

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const contact = () => {
+  return (
+    <div>
+      <div>Hello world!</div>
+      <div>
+        <Link to="/">Front Page</Link>
+      </div>
+    </div>
+  )
+}
+export default contact
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import { Link } from "gatsby"
+
+const Index = () => {
+  return (
+    <div>
+      <div>It only takes a few minutes to get up and running!</div>
+      <div></div>
+      <div>
+        <Link to="/contact/">Contact</Link>
+      </div>
+    </div>
+  )
+}
+export default Index
