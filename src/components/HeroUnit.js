4e8a4a445b33b217dff3df724496b67e5f8a67fa
Ola Vea <ola@olavea.com>
2019-11-26 08:01:15 +0100

    import Typography from '@material-ui/core/Typography'; look like https://material-ui.com/getting-started/templates/album/ #6

--- src/components/HeroUnit.js
+++ src/components/HeroUnit.js
@@ -2,27 +2,41 @@ import React from "react"
 import { Button } from "@material-ui/core"
 import ArrowForward from "@material-ui/icons/ArrowForward"
 import Grid from "@material-ui/core/Grid"
+import Container from "@material-ui/core/Container"
+import Typography from "@material-ui/core/Typography"
+
 //aria-label="ArrowForward"
 const HeroUnit = () => {
   return (
     <div>
-      <div>What is, who for and why useAuth?</div>
-      <div>
+      <Container maxWidth="sm">
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          What is useAuth?
+        </Typography>
+        <div>For who for and why useAuth?</div>
         <div>
-          <Grid container justify="center">
-            <Button
-              size="large"
-              variant="contained"
-              color="primary"
-              href="/docs/"
-              overrideCSS={{ mt: 5 }}
-              endIcon={<ArrowForward />}
-            >
-              Get Started
-            </Button>
-          </Grid>
+          <div>
+            <Grid container justify="center">
+              <Button
+                size="large"
+                variant="contained"
+                color="primary"
+                href="/docs/"
+                overrideCSS={{ mt: 5 }}
+                endIcon={<ArrowForward />}
+              >
+                Get Started
+              </Button>
+            </Grid>
+          </div>
         </div>
-      </div>
+      </Container>
     </div>
   )
 }
